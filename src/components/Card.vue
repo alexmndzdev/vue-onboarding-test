623f4fe4ae0ecf7d8bbe5b013157de6d7dc47a40
alexmndzdev <alejandro.mendez.developer@gmail.com>
2021-04-12 00:39:41 -0500

    Home finished responsive design

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -17,7 +17,26 @@ export default {
 
 <style lang="scss" scoped>
 @import '../assets/scss/_variables.scss';
+@import '../assets/scss/mixins.scss';
   .card {
+    display: flex;
+    flex-direction: column;
+    flex-basis: 100%;
+    flex: 1;
+    margin-bottom: 20px;
+    @include sm {
+      min-width: 150px;
+      padding: 10px 50px;
+      margin: 10px 50px;
+    }
+    @include lg {
+      min-width: 300px;
+    }
+    img {
+      margin: auto;
+      border-radius: 500px;
+      background-color: $font-color-secondary;
+    }
     #title {
       color: $font-color-primary;
     }
