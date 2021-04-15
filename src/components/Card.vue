3f355dce9c3201191bf0661dfae3be665c093c46
alexmndzdev <alejandro.mendez.developer@gmail.com>
2021-04-15 01:33:23 -0500

    Added sidebar updating info

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,24 +1,93 @@
 <template>
-  <div class="card">
+  <div
+    class="card"
+    :class="{'selectable': selectable}"
+    >
     <img :src="picture" alt="picture" id="profile-picture">
-    <h2 id="title">{{ fullName }}</h2>
-    <p id="description">
-    <!-- eslint-disable-line -->
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam, facilis exercitationem
+    <div v-if="!selectable" class="editable">
+      <h2 id="title" v-show="!editingName">{{ currentName }}</h2>
+      <span v-show="editingName">
+        <input
+          v-model="first_name"
+          type="text"
+          @keydown.enter="editingName=false;updateUser({ first_name })"
+          @blur="editingName=false;updateUser({ first_name })"
+          >
+      </span>
+      <span @click="editingName=true" class="material-icons edit">edit</span>
+    </div>
+    <h1 id="title" v-else>
+      {{ userData.first_name }}
+    </h1>
+    <div v-if="!selectable" class="editable">
+      <span id="description" v-show="!editing">
+        {{ currentEmail }}
+      </span>
+      <span v-show="editing">
+        <input
+          v-model="email"
+          type="text"
+          @keydown.enter="editing=false;updateUser({ email })"
+          @blur="editing=false;updateUser({ email })"
+          >
+      </span>
+      <span @click="editing=true" class="material-icons edit">edit</span>
+    </div>
+    <p v-else id="description">
+      {{ userData.email }}
     </p>
   </div>
 </template>
 
 <script>
+import axios from '../axios';
+
 export default {
   name: 'Card',
-  props: ['userData'],
+  props: ['userData', 'selectable'],
+  data() {
+    return {
+      editing: false,
+      editingName: false,
+      email: '',
+      first_name: '',
+    };
+  },
   computed: {
     fullName() {
       return this.userData ? `${this.userData.first_name} ${this.userData.last_name}` : 'Nombre Usuario';
     },
     picture() {
       return this.userData?.avatar || 'https://www.gravatar.com/avatar/default?s=200&r=pg&d=mm';
+    },
+    currentName() {
+      if (this.first_name.length > 0) {
+        return this.first_name;
+      }
+      return this.userData.first_name;
+    },
+    currentEmail() {
+      if (this.email.length > 0) {
+        return this.email;
+      }
+      return this.userData.email;
+    },
+  },
+  methods: {
+    updateUser(obj) {
+      axios.RE.put(`user/${this.userData.id}`, obj);
+      const updatedUser = { ...this.userData };
+      updatedUser.email = this.currentEmail;
+      updatedUser.first_name = this.currentName;
+      this.$emit('updatedUser', updatedUser);
+    },
+  },
+  watch: {
+    userData(oldVal, newVal) {
+      if (newVal) {
+        this.email = this.userData.email;
+        this.first_name = this.userData.first_name;
+      }
     },
   },
 };
@@ -35,17 +104,18 @@ export default {
     flex: 1;
     margin-bottom: 20px;
     border-radius: 4px;
-    background: #fff;
-    box-shadow: 0 6px 10px rgba(0,0,0,.08), 0 0 6px rgba(0,0,0,.05);
-    // eslint-disable-line max-len
-    transition: .3s transform cubic-bezier(.155,1.105,.295,1.12),
-    .3s box-shadow,
-    .3s -webkit-transform cubic-bezier(.155,1.105,.295,1.12);
-    padding: 14px 80px 18px 36px;
-    cursor: pointer;
-    &:hover {
-      transform: scale(1.05);
-      box-shadow: 0 10px 20px rgba(0,0,0,.12), 0 4px 8px rgba(0,0,0,.06);
+    &.selectable {
+      background: #fff;
+      box-shadow: 0 6px 10px rgba(0,0,0,.08), 0 0 6px rgba(0,0,0,.05);
+      transition: .3s transform cubic-bezier(.155,1.105,.295,1.12),
+      .3s box-shadow,
+      .3s -webkit-transform cubic-bezier(.155,1.105,.295,1.12);
+      padding: 14px 80px 18px 36px;
+      cursor: pointer;
+      &:hover {
+        transform: scale(1.05);
+        box-shadow: 0 10px 20px rgba(0,0,0,.12), 0 4px 8px rgba(0,0,0,.06);
+      }
     }
     @include sm {
       min-width: 150px;
@@ -65,6 +135,14 @@ export default {
     }
     #subtitle {
       color: $font-color-secondary;
+    }
+    span.material-icons.edit{
+      cursor: pointer;
+    }
+    .editable {
+      display: flex;
+      justify-content: center;
+      align-items: center;
     }
   }
 </style>
--- src/views/layouts/Main.vue
+++ src/views/layouts/Main.vue
@@ -1,10 +1,25 @@
 <template>
   <div class="main">
     <Navbar />
-    <div class="container">
+    <div id="container" class="container">
       <template v-for="user in users" >
-        <Card :key="user.id" :user-data="user" />
+        <Card
+          :key="user.id"
+          :user-data="user"
+          @click.native="showSidebar(user)"
+          :selectable="true"
+          :ref="'card-'+user.id"
+        />
       </template>
+    </div>
+    <div id="side-menu">
+      <a class="closebtn" href="#" @click="closeSidebar">
+        <span class="material-icons">close</span>
+      </a>
+      <Card
+        :user-data="selectedUser"
+        @updatedUser="onUpdatedUser($event)"
+      />
     </div>
   </div>
 </template>
@@ -22,6 +37,7 @@ export default {
   },
   data() {
     return {
+      selectedUser: {},
       users: [],
     };
   },
@@ -29,9 +45,25 @@ export default {
     this.getUsers();
   },
   methods: {
+    onUpdatedUser(obj) {
+      // this.getUsers(); is removed cause the api is not saving changes
+      /* eslint no-underscore-dangle: ["error", { "allow": ["_props"] }] */
+      const userCard = this.$refs[`card-${obj.id}`][0]._props.userData;
+      userCard.email = obj.email;
+      userCard.first_name = obj.first_name;
+    },
     async getUsers() {
       const { data } = await axios.RE.get('users');
       this.users = data.data;
+    },
+    showSidebar(user) {
+      this.selectedUser = user;
+      document.getElementById('container').style.marginRight = '35%';
+      document.getElementById('side-menu').style.width = '35%';
+    },
+    closeSidebar() {
+      document.getElementById('container').style.marginRight = '0px';
+      document.getElementById('side-menu').style.width = '0px';
     },
   },
 };
@@ -50,6 +82,25 @@ div.main {
       flex-wrap: wrap;
       justify-content: space-between;
       width: 100%;
+    }
+  }
+  div#side-menu{
+    height: 100%;
+    width: 0;
+    position: absolute;
+    z-index: 1;
+    top: 6em;
+    right: 0;
+    background: #fff;
+    overflow-x: hidden;
+    transition: 0.5s;
+    padding-top: 60px;
+    border: 1px solid $font-color-secondary;
+    .closebtn {
+      position: absolute;
+      top: 0;
+      right: 25px;
+      margin-left: 50px;
     }
   }
 }
