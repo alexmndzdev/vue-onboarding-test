f2896f4aa52eb96dc975ad08fbbd824e287e0921
alexmndzdev <alejandro.mendez.developer@gmail.com>
2021-04-15 12:18:50 -0500

    Refactor Card tests

--- src/components/Card.spec.js
+++ src/components/Card.spec.js
@@ -2,8 +2,19 @@ import { shallowMount } from '@vue/test-utils';
 import Card from './Card.vue';
 
 describe('Card', () => {
+  const userData = {
+    id: 7,
+    email: 'michael.lawson@reqres.in',
+    first_name: 'Michael',
+    last_name: 'Lawson',
+    avatar: 'https://reqres.in/img/faces/7-image.jpg',
+  };
   it('Has the required elements', async () => {
-    const wrapper = shallowMount(Card);
+    const wrapper = shallowMount(Card, {
+      propsData: {
+        userData,
+      },
+    });
     const findPicture = () => wrapper.find('#profile-picture');
     expect(findPicture().exists()).toBe(true);
 
@@ -14,20 +25,13 @@ describe('Card', () => {
     expect(findDescription().exists()).toBe(true);
   });
   it('Is getting user correctly', () => {
-    const userData = {
-      id: 7,
-      email: 'michael.lawson@reqres.in',
-      first_name: 'Michael',
-      last_name: 'Lawson',
-      avatar: 'https://reqres.in/img/faces/7-image.jpg',
-    };
     const wrapper = shallowMount(Card, {
       propsData: {
         userData,
       },
     });
     const findTitle = () => wrapper.find('#title');
-    expect(findTitle().text()).toBe('Michael Lawson');
+    expect(findTitle().text()).toBe('Michael');
     const findPicture = () => wrapper.find('#profile-picture');
     expect(findPicture().attributes('src')).toBe(userData.avatar);
   });
