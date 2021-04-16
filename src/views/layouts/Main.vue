75b80908622e81ddf76d0f44047acbd63a47635f
alexmndzdev <alejandro.mendez.developer@gmail.com>
2021-04-15 22:01:49 -0500

    Added sidebar with table & tests

--- src/views/layouts/Main.vue
+++ src/views/layouts/Main.vue
@@ -12,7 +12,7 @@
         />
       </template>
     </div>
-    <div id="side-menu">
+    <div id="side-menu" :class="{'opened': clickedCard}">
       <a class="closebtn" href="#" @click="closeSidebar">
         <span class="material-icons">close</span>
       </a>
@@ -20,11 +20,28 @@
         :user-data="selectedUser"
         @updatedUser="onUpdatedUser($event)"
       />
+      <br>
+      <table-component
+         :data="userPosts"
+         sort-by="title"
+         sort-order="asc"
+      >
+        <table-column show="id" label="Id" data-type="numeric"/>
+        <table-column show="title" label="Title"/>
+        <table-column :sortable="false" :filterable="false">
+          <template slot-scope="userPosts">
+            <a href="#" @click="removePostById(userPosts.id)">
+                <span class="material-icons">delete</span>
+            </a>
+          </template>
+      </table-column>
+     </table-component>
     </div>
   </div>
 </template>
 
 <script>
+import { TableComponent, TableColumn } from 'vue-table-component';
 import Navbar from '../../components/Navbar.vue';
 import Card from '../../components/Card.vue';
 import axios from '../../axios';
@@ -34,17 +51,28 @@ export default {
   components: {
     Navbar,
     Card,
+    TableComponent,
+    TableColumn,
   },
   data() {
     return {
       selectedUser: {},
       users: [],
+      userPosts: [],
+      clickedCard: false,
     };
   },
   beforeMount() {
     this.getUsers();
   },
   methods: {
+    removePostById(postId) {
+      axios.TC.delete(`posts/${postId}`).then((res) => {
+        if (res.status === 200) {
+          this.userPosts = this.userPosts.filter((post) => post.id !== postId);
+        }
+      });
+    },
     onUpdatedUser(obj) {
       // this.getUsers(); is removed cause the api is not saving changes
       /* eslint no-underscore-dangle: ["error", { "allow": ["_props"] }] */
@@ -53,17 +81,20 @@ export default {
       userCard.first_name = obj.first_name;
     },
     async getUsers() {
-      const { data } = await axios.RE.get('users');
+      const { data } = await axios.RE.get('users?page=2');
       this.users = data.data;
+    },
+    async getPostsByUserid() {
+      const { data } = await axios.TC.get(`posts?user_id=${this.selectedUser.id}`);
+      this.userPosts = data;
     },
     showSidebar(user) {
       this.selectedUser = user;
-      document.getElementById('container').style.marginRight = '35%';
-      document.getElementById('side-menu').style.width = '35%';
+      this.getPostsByUserid();
+      this.clickedCard = true;
     },
     closeSidebar() {
-      document.getElementById('container').style.marginRight = '0px';
-      document.getElementById('side-menu').style.width = '0px';
+      this.clickedCard = false;
     },
   },
 };
@@ -73,6 +104,12 @@ export default {
 @import '../../assets/scss/_variables.scss';
 @import '../../assets/scss/mixins.scss';
 div.main {
+  position: relative;
+  overflow: hidden;
+  height: 100%;
+  @include sm {
+    height: 100vh;
+  }
   div.container{
     padding-top: 20px;
     display: block;
@@ -85,22 +122,34 @@ div.main {
     }
   }
   div#side-menu{
-    height: 100%;
-    width: 0;
-    position: absolute;
+    width: 100%;
+    position: fixed;
+    @include md {
+      width: 50%;
+      position: absolute;
+    };
+    @include lg {
+      width: 35%;
+      position: absolute;
+    }
     z-index: 1;
     top: 6em;
-    right: 0;
     background: #fff;
-    overflow-x: hidden;
-    transition: 0.5s;
     padding-top: 60px;
     border: 1px solid $font-color-secondary;
+    box-shadow: 5px 5px 10px grey;
+    opacity: 0;
+    right: -100%;
+    transition: all 0.50s;
+    -webkit-transition: all 0.50s;
+    &.opened {
+      opacity: 1;
+      right: 0;
+    }
     .closebtn {
       position: absolute;
-      top: 0;
+      top: 10px;
       right: 25px;
-      margin-left: 50px;
     }
   }
 }
